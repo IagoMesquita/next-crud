6c4fc533ad6515e09fd3c93278737a35723d3863
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-10-21 13:38:53 -0300

    Feat: component Input

---
+++ src/components/Input.tsx
@@ -0,0 +1,30 @@
+interface InputProps {
+  type?: 'text' | 'number'
+  text: string
+  value: any
+  onlyReading?: boolean
+  onChange?: (value: any) => void
+  className?: string
+}
+
+export default function Input(props: InputProps) {
+  return (
+    <div className={`flex flex-col  ${props.className}`}>
+      <label className="mb-2">
+        {props.text}
+      </label>
+      <input
+        type={props.type ?? 'text'} 
+        onChange={(e) => props.onChange?.(e.target.value)}
+        value={props.value}
+        readOnly={props.onlyReading}
+        className={`
+          border border-purple-500 rounded-lg
+          focus:outline-none bg-gray-50
+          px-4 py-2
+          ${props.onlyReading ? '' : 'focus:bg-white'}
+        `}
+      />
+    </div>
+  )
+}
