c0f25aded7e98f63750702c1a3fa7fcf43ebc507
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-10-25 20:02:01 -0300

    refactor: use hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,58 +1,25 @@
 import { Inter } from 'next/font/google'
 import Layout from '@/components/Layout'
-import Client from '@/core/Client'
 import Table from '@/components/Table'
 import Button from '@/components/Button'
 import Form from '@/components/Form'
-import { useEffect, useState } from 'react'
-import { ClientRepository } from '@/core/ClientRepository'
-import ClientCollection from '@/backend/db/ClientCollection'
+
+import useClients from '@/hooks/useClients'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-  const repoClients: ClientRepository = new ClientCollection()
-
-  const [clients, setClients] = useState<Client[]>([])
-  const [client, setClient] = useState<Client>(Client.empty)
-  const [isVisible, setIsVisible] = useState<'table' | 'form'>('table')
-
-  // useEffect(() => {
-  //   repoClients.getAll().then(setClients)
-  //   console.log(clients)
-  // }, [])
-  
-  useEffect(getClients, [])
-
-  function getClients() {
-    repoClients.getAll().then((clients) => {
-      setClients(clients)
-      setIsVisible('table')
-    });
-  }
-
-  const saveClient = async (client: Client) => {
-    await repoClients.save(client)
-    getClients()
-  }
-
-  const delectedClient = async (client: Client) => {
-    await repoClients.delete(client)
-    getClients()
-  }
-
-  const selectedClient = (client: Client) => {
-    setClient(client)
-    setIsVisible('form')
-  }
-
-
-  const newClient = () => {
-    setClient(Client.empty())
-    setIsVisible('form')
-  }
-
+  const {
+    client,
+    clients,
+    newClient,
+    selectedClient,
+    delectedClient,
+    saveClient,
+    visibleTable,
+    displayTable
+  } = useClients()
 
 
 
@@ -63,7 +30,7 @@ export default function Home() {
     text-white 
     `}>
       <Layout title='Cadastro Simples'>
-        {isVisible === 'table' ? (
+        {visibleTable ? (
           <>
             <div className='flex justify-end'>
               <Button
@@ -82,7 +49,7 @@ export default function Home() {
           </>
         ) : (
           <Form
-            cancel={() => setIsVisible('table')}
+            cancel={displayTable}
             changeClient={saveClient}
             client={client}
           />
